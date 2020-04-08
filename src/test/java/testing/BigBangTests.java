de85e3ebb82de0b100e8a2682bf497c715ece0d1
Diana Noveanu <diana.noveanu@teleporthq.io>
2020-04-08 13:04:28 +0300

    solve test

--- src/test/java/testing/BigBangTests.java
+++ src/test/java/testing/BigBangTests.java
@@ -1,8 +1,7 @@
 package testing;
 
-import org.junit.AfterClass;
+import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import testing.domain.Nota;
 import testing.domain.Student;
@@ -16,6 +15,7 @@ import testing.validation.StudentValidator;
 import testing.validation.TemaValidator;
 import testing.validation.Validator;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
@@ -32,7 +32,7 @@ public class BigBangTests {
     private Service service;
 
     @Before
-    public void setup() {
+    public void setup() throws FileNotFoundException {
         Validator<Student> vs = new StudentValidator();
         Validator<Nota> ns = new NotaValidator();
         Validator<Tema> ts = new TemaValidator();
@@ -42,10 +42,38 @@ public class BigBangTests {
         temaXMLRepository = new TemaXMLRepository(ts, "src/test/java/testing/teme.xml");
         service = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);
 
+
+        PrintWriter writer = new PrintWriter("src/test/java/testing/studenti.xml");
+        writer.print("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
+                "<Entitati>\n" +
+                "    <student ID=\"1000\">\n" +
+                "        <Nume>Diana</Nume>\n" +
+                "        <Grupa>935</Grupa>\n" +
+                "    </student>\n" +
+                "</Entitati>\n");
+        writer.close();
+
+        writer = new PrintWriter("src/test/java/testing/teme.xml");
+        writer.print("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
+                "<Entitati>\n" +
+                "<tema ID=\"1000\">\n" +
+                "<Descriere>File</Descriere>\n" +
+                "<Deadline>7</Deadline>\n" +
+                "<Startline>6</Startline>\n" +
+                "</tema>\n" +
+                "</Entitati>\n");
+        writer.close();
+
+        writer = new PrintWriter("src/test/java/testing/note.xml");
+        writer.print("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
+                "<Entitati>\n" +
+                "</Entitati>\n");
+        writer.close();
+
     }
 
-    @BeforeClass
-    public static void tearDown() {
+    @After
+    public void tearDown() {
         try {
             String defaultFileContent = new String(Files.readAllBytes(Paths.get("src/test/java/testing/defaultFile.xml")), StandardCharsets.UTF_8);
 
@@ -64,26 +92,6 @@ public class BigBangTests {
         }
     }
 
-
-    @AfterClass
-    public static void tearDown1() {
-        try {
-            String defaultFileContent = new String(Files.readAllBytes(Paths.get("src/test/java/testing/defaultFile.xml")), StandardCharsets.UTF_8);
-
-            PrintWriter printWriter = new PrintWriter("src/test/java/testing/studenti.xml");
-            PrintWriter printWriter1 = new PrintWriter("src/test/java/testing/note.xml");
-            PrintWriter printWriter2 = new PrintWriter("src/test/java/testing/teme.xml");
-
-            printWriter.print(defaultFileContent);
-            printWriter1.println(defaultFileContent);
-            printWriter2.println(defaultFileContent);
-            printWriter.close();
-            printWriter1.close();
-            printWriter2.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     @Test
     public void testAddAssignment(){
@@ -97,14 +105,14 @@ public class BigBangTests {
 
     @Test
     public void testAddGrade(){
-        assertEquals(service.saveNota("1", "1", 9, 3, "good"), 1);
+        assertEquals(service.saveNota("1000", "1000", 9, 3, "good"), -1);
     }
 
     @Test
     public void testBigBang(){
         assertEquals(service.saveStudent("2", "Alexandru", 231), 1);
         assertEquals(service.saveTema("2", "1", 8, 7), 1);
-        assertEquals(service.saveNota("2", "2", 9, 3, "good"), 1);
+        assertEquals(service.saveNota("2", "2", 9, 3, "good"), 1);.
 
     }
 }
